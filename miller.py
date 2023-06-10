fad0e52e5072dd4e1a7cf5f98508c04c3bcc9a43
ernaniraffo <ernani.raffo@icloud.com>
2023-06-09 22:45:29 -0700

    printing Miller

--- miller.py
+++ miller.py
@@ -4,23 +4,21 @@ from scipy import constants
 
 
 def main():
-    earth = Planet("Earth", 1)
-    sun = MassiveObject("Sun", 1)
-
     gargantua = MassiveObject("Gargantua", 100000000)
 
     # figure out distance between Miller and Gargantua
     r = (2 * constants.G * gargantua.mass / constants.c**2)
     event_horizon = r
+    print("Gargantua's event horizon:", event_horizon / constants.au, "AU")
     miller_time = 0
     while miller_time < constants.hour:
         miller = Planet("Miller", r / constants.au)
         miller_time = time_dilation(constants.year * 7, miller, gargantua)
         r += 1
 
-    print(earth)
-    print(sun)
     print(gargantua)
+    print(miller)
+    print("Miller is", miller.r - event_horizon, "meters away from Gargantua")
     print("7 years on Earth is", miller_time / constants.hour, "hours on Miller's planet")
 
 
